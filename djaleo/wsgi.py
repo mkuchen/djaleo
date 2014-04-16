ff79d1eb3333fada4625564f15cc6fc710d5ede2
Mitch <mkuchen@umich.edu>
2014-04-16 13:45:44 -0400

    initial commit

---
+++ djaleo/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for djaleo project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+from dj_static import Cling
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djaleo.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = Cling(get_wsgi_application())
