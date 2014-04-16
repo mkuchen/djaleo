1b2a2cb3c534aba47f5a31df7767d930e3fe6247
Mitch <mkuchen@umich.edu>
2014-04-16 14:17:56 -0400

    getting through some errors...

--- djaleo/wsgi.py
+++ djaleo/wsgi.py
@@ -6,9 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-from dj_static import Cling
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djaleo.settings")
 
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 application = Cling(get_wsgi_application())
