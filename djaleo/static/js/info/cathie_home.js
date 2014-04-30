b8ac9cb8e8b1d02315f7c444acef4230ef663fd5
Mitch <mkuchen@umich.edu>
2014-04-30 16:24:33 -0400

    top nav bar is now ready

--- djaleo/static/js/info/cathie_home.js
+++ djaleo/static/js/info/cathie_home.js
@@ -1,13 +1,14 @@
 $(function(){
-    $('#header-nav').data('size','big');
+    $('#sliding-nav').data('size','big');
 });
 
 $(window).scroll(function(){
-    var $navOne = $('#header-nav');
+    var $navOne = $('#sliding-nav');
     var $navTwo = $('.cath-main-nav');
     if ($('body').scrollTop() > 50) {
         if ($navOne.data('size') == 'big') {
             $navOne.css("position","fixed");
+            $navOne.addClass("sliding");
             $navOne.data('size','small').stop().animate({
                 height:'60px',
                 top:'0'
@@ -19,11 +20,12 @@ $(window).scroll(function(){
     } else {
         if ($navOne.data('size') == 'small') {
             $navOne.css("position","relative");
+            $navOne.removeClass("sliding");
             $navOne.data('size','big').stop().animate({
                 height:'112px',
             }, 600);
             $navTwo.data('size','big').stop().animate({
-                "padding-top":"28px",
+                "padding-top":"22px",
             }, 600);
         }  
     }
